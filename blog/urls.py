12125bebe7767a0176d715a6b316d1b0b3438a60
Eugene <ugence@gmail.com>
2022-10-05 20:22:15 +0000

    added post list, added link to post link

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 
 urlpatterns = [
 
-    path('<slug:slug>', views.Blogview.as_view(), name='blog_view')
+    path('<slug:slug>', views.Blogview.as_view(), name='blog_view'),
+    path('about/', views.AboutView.as_view(), name='about_view'),
+    path('', views.PostList.as_view(), name='home')
 ]
--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,16 @@ from django.views import generic
 
 # Create your views here.
 
+
 class Blogview(generic.DetailView):
     model = Post
     template_name = 'blog.html'
+
+
+class AboutView(generic.TemplateView):
+    template_name = 'about.html'
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-date_created')
+    template_name = 'index.html'
